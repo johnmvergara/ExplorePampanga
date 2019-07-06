c3c8921997910ea5040939a197d925628178c345
johnmvergara <johnmichael.vergara.iics@ust.edu.ph>
2019-07-07 01:16:00 +0800

    Initial commit

--- app/src/main/java/com/example/explorepampanga/MainActivity.java
+++ app/src/main/java/com/example/explorepampanga/MainActivity.java
@@ -5,19 +5,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    EditText txtName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtName = findViewById(R.id.etName);
     }
 
     public void goProceed(View v){
         Intent i = new Intent(this, Menu.class);
         startActivity(i);
         Toast.makeText(this, "Going to Menu", Toast.LENGTH_LONG).show();
+        String name = txtName.getText().toString();
+        i.putExtra("person", name);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/explorepampanga/Menu.java
+++ app/src/main/java/com/example/explorepampanga/Menu.java
@@ -2,17 +2,25 @@ package com.example.explorepampanga;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Menu extends Activity {
+
+    TextView txtGreeting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        txtGreeting = findViewById(R.id.tvGreeting);
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("person");
+        txtGreeting.setText("Welcome, " + str);
     }
 
     public void goTourist(View v){
@@ -25,5 +33,17 @@ public class Menu extends Activity {
         Intent i = new Intent(this, delicacies.class);
         startActivity(i);
         Toast.makeText(this, "Delicacies", Toast.LENGTH_LONG).show();
+    }
+
+    public void goHistory(View v){
+        Intent i = new Intent(this, History.class);
+        startActivity(i);
+        Toast.makeText(this, "History", Toast.LENGTH_LONG).show();
+    }
+
+    public void goDirectory(View v){
+        Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse("https://lga.gov.ph/province/info/pampanga"));
+        startActivity(i);
+        Toast.makeText(this, "Directory", Toast.LENGTH_LONG).show();
     }
 }
